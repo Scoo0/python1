eb37e3d2e6d6ea6c41f2c18dd144443e897b96a3
amin <amin137718@gmail.com>
2019-05-17 10:37:43 +0330

    try weight 2

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-weight = 80
+weight = 90
 height = 1.80
 bmi = weight /(height**2)
 print (bmi)
 if 18 < bmi < 25 :
     print("normal")
-if bmi > 25 :
-    print("over")
+else:
+    print("not normal")
+
